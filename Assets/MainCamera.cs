3b6d3ca84de05a1189a21089fe2bb49fa5aca63d
Fiadh Willow <10160927+hyacynthesizer@users.noreply.github.com>
2022-09-05 18:34:22 -0700

    Double Camera Time
    Basic controls for camera, plus confirming that the viewport works and spins correctly

--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    public float horizVel = 10f;
+    public float vertVel = 10f;
+    public float panVel = 20f;
+    public float pitchVel = 20f;
+    
     void Start()
     {
         transform.position = new Vector3(0,2,-2);
+        transform.rotation = Quaternion.identity;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // read keys
+        float horizTrans = Input.GetAxis("Vertical") * horizVel; // forward and back
+        float vertTrans = Input.GetAxis("Horizontal") * vertVel; // will need to include up-down motion later
+        float horizRot = Input.GetAxis("Pan") * panVel; // controlled with J & L
+        float vertRot = Input.GetAxis("Pitch") * pitchVel; // controlled with I & K
+        float reset = Input.GetAxis("Fire1"); // snap camera to no X or Z rotation
+        
+        var dt = Time.deltaTime;
+        horizTrans *= dt;
+        vertTrans *= dt;
+        horizRot *= dt;
+        vertRot *= dt;
+        
+        transform.Translate(vertTrans, 0, horizTrans);
+        transform.Rotate(-vertRot, horizRot, 0);
+        
+        if (reset>0f)
+        {
+            var anglesAdjust = transform.rotation.eulerAngles;
+            anglesAdjust.y = 0;
+            anglesAdjust *= -1;
+//            print(transform.rotation.eulerAngles);
+//            print(anglesAdjust);
+            transform.Rotate(anglesAdjust);
+        }
         
     }
 }
