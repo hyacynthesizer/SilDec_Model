278f2e0af5af6584c38211b140a3d78be1634d0a
Fiadh Willow <10160927+hyacynthesizer@users.noreply.github.com>
2022-09-06 00:04:14 -0700

    Planet Data
    Plus outlines for what needs to be implemented next regarding getting all the planets to move

--- Assets/BodiesHandler.cs
+++ Assets/BodiesHandler.cs
@@ -9,11 +9,17 @@ public class BodiesHandler : MonoBehaviour
     void Start()
     {
         transform.position = new Vector3(0,0,0);
+        
+        // read in CSV of raw planetary data
+        // feed into dictionary of bodies and attributes
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // command to reset all planets to their base positions
+        // process for relocating world when camera-origin distance will cause float issues
+        // process to adjust planet positions as a function of time
+        // keeping track of world time in terms of earth days and system calendar
     }
 }
